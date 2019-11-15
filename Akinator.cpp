0c64469338f3fe862f1126a457dfa12149dd94b2
ildarovadam <ildarovadam@gmail.com>
2019-11-15 03:02:38 +0300

    Added gamemods

--- Akinator.cpp
+++ Akinator.cpp
@@ -1,12 +1,12 @@
 #include "Akinator.hpp"
 
-void ask_question(Tree::tree_t* answers, Tree::node_t* cur){
+void ask_question(Tree::tree_t* answers, Tree::node_t* cur, FILE* data_source){
 	
 	Assert(answers != NULL);
 	Assert(cur != NULL);
 
 	if (!cur->has_left && !cur->has_rigth){
-		make_guess(answers, cur);
+		make_guess(answers, cur, data_source);
 		return;
 	}
 
@@ -15,16 +15,16 @@ void ask_question(Tree::tree_t* answers, Tree::node_t* cur){
 	char resp[MAX_BUFF_SIZE] = ""; 
 	fgets(resp, MAX_BUFF_SIZE, stdin);
 
-	if (strstr(resp, "Yes") != NULL){
-		ask_question(answers, cur->left);
+	if (strstr(resp, "yes") != NULL){
+		ask_question(answers, cur->left, data_source);
 	} else {
-		ask_question(answers, cur->right);
+		ask_question(answers, cur->right, data_source);
 	}
 
 }
 
 
-void make_guess(Tree::tree_t* answers, Tree::node_t* leaf){
+void make_guess(Tree::tree_t* answers, Tree::node_t* leaf, FILE* data_source){
 	
 	Assert(answers != NULL);
 	Assert(leaf != NULL);
@@ -34,15 +34,15 @@ void make_guess(Tree::tree_t* answers, Tree::node_t* leaf){
 	char resp[MAX_BUFF_SIZE] = ""; 
 	fgets(resp, MAX_BUFF_SIZE, stdin);
 
-	if (strstr(resp, "да") != NULL){
+	if (strstr(resp, "yes") != NULL){
 		printf("Akk: I knew it!!!\n");
 	} else {
-		add_new_character(answers, leaf);	
+		add_new_character(answers, leaf, data_source);	
 	}
 }
 
 
-void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character){
+void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character, FILE* data_source){
 
 	Assert(answers != NULL);
 	Assert(old_character != NULL);
@@ -53,7 +53,7 @@ void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character){
 	getchar();
 
 	Tree::node_t* new_question = NEW_NODE;
-	printf("Akk: I don't really know what is the difference between %s and %s?\n", new_character->val, old_character->val);
+	printf("Akk: I don't really know this guy, what is the difference between %s and %s?\n", new_character->val, old_character->val);
 	scanf("%m[^\n]", &new_question->val);
 	getchar();
 
@@ -68,7 +68,11 @@ void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character){
 	Tree::attach_left(answers, new_question, new_character);
 	Tree::attach_right(answers, new_question, old_character);
 
+	data_source = freopen(tree_filename, "w", data_source);
 	Tree::save_to_file(answers, data_source);
+	fclose(data_source);
+
+	printf("Akk: Thank you, even genius should learn something new.\n");
 
 }
 
@@ -96,43 +100,176 @@ int list_vals_from_node_to_ancestor(Tree::node_t* cur, Tree::node_t* ancestor, T
 }
 
 
-Tree::node_t* find_common_ancestor(Tree::node_t* first, Tree::node_t* second){
-	
-	Assert(first != NULL);	
-	Assert(second != NULL);	
+void print_definition(Tree::node_t* cur, Tree::node_t* finish, bool* flag){
 
-	while (first->level > second->level){
-		first = first->parent;
+	Assert(cur != NULL);
+
+	if (cur->parent == NULL){
+		return;
 	}
 
-	while (second->level > first->level){
-		second = second->parent;
+	if (cur->parent != finish){
+		print_definition(cur->parent, finish, flag);	
 	}
 
-	while (first != second){
-		first = first->parent;
-		second = second->parent;
+
+	if (cur == cur->parent->left){
+		printf("%s, ", cur->parent->val);
+		*flag = 1;
 	}
 
-	return first;
+	if (cur == cur->parent->right){
+		printf("not %s, ", cur->parent->val);
+		*flag = 1;
+	}
+
 }
 
+void guessing_mode(){
 
-int main(){
 	Tree::tree_t answers = {}; 
 
 	char resp[MAX_BUFF_SIZE] = ""; 
 
 	do {
-		fseek(data_source, 0, SEEK_SET);
-
+		FILE* data_source = fopen(tree_filename, "r");
 		answers = Tree::read_from_file(data_source);
-		ask_question(&answers, answers.root);
 
-		printf("Akk: Do you wanna play again?\n");
-		fgets(resp, MAX_BUFF_SIZE, stdin);
-		if (strstr(resp, "no") != NULL) break;
-	} while (true);
+		ask_question(&answers, answers.root, data_source);
+		fclose(data_source);
+
+		printf("press e to quit to the menu\n");
+
+	} while (getchar() != 'e');
+
+	Tree::destruct(answers.root);
+}
+
+
+void definition_mode(){
+
+	Tree::tree_t answers = {}; 
+
+	char resp[MAX_BUFF_SIZE] = ""; 
+	bool flag = 0;
+
+	do {
+		FILE* data_source = fopen(tree_filename, "r");
+		answers = Tree::read_from_file(data_source);
+
+		Tree::node_t* desired = {};
+
+		do {
+			printf("Difinition of what you wanna know?\n");
+			scanf("%[^\n]", resp);
+			getchar();
+			desired = Tree::find_node_by_val(answers.root, resp, strcmp, answers.size);	
+		} while (desired == NULL);
+
+		printf("Akk: ");
+		print_definition(desired, answers.root, &flag);
+		printf("\n\n");
+
+		fclose(data_source);
+
+		printf("press e to quit to the menu\n");
+
+	} while (getchar() != 'e');
+}
+
+
+void comaparison_mode(){
+
+	Tree::tree_t answers = {}; 
+
+	char resp[MAX_BUFF_SIZE] = ""; 
+
+	Tree::node_t* first = {};
+	Tree::node_t* second = {};
+	Tree::node_t* common = {};
+
+	bool has_property;
+
+	do {
+		FILE* data_source = fopen(tree_filename, "r");
+		answers = Tree::read_from_file(data_source);
+
+
+		do {
+			printf("What do you want to compare?\n");
+			scanf("%[^\n]", resp);
+			getchar();
+
+			first = Tree::find_node_by_val(answers.root, resp, strcmp, answers.size);	
+			if (first == NULL) continue;
+		
+			printf("What do you want to compare %s with?\n", first->val);
+			scanf("%[^\n]", resp);
+			getchar();
+		
+			second = Tree::find_node_by_val(answers.root, resp, strcmp, answers.size);	
+			if (second == NULL) continue;
+
+			common = Tree::find_common_ancestor(first, second);	
+		} while (first == NULL || second == NULL);
+
+
+		printf("Actually, %s and %s have in common: ", first->val, second->val);
+		print_definition(common, answers.root, &has_property);
+		if (!has_property){
+			printf("nothing, ");
+		}
+
+		printf("however, %s is: ", first->val);
+		print_definition(first, common, &has_property);
+		printf("when %s is: ", second->val);
+		print_definition(second, common, &has_property);
+		printf("I am certanly a genius.\n");
+		fclose(data_source);
+
+		printf("press e to quit to the menu\n");
+		
+	} while (getchar() != 'e');
+
+	destruct(answers.root);
+}
+
+void show_tree(){
+	Tree::tree_t answers = {}; 
+	FILE* data_source = fopen(tree_filename, "r");
+
+	answers = Tree::read_from_file(data_source);
+	draw(&answers, 0);
 
 	fclose(data_source);
+}
+
+int main(){	
+		printf("Akk: Hello dear, come in, you'll be my guest\n");
+		int choice = 0;
+	do {
+		printf("Akk: Do you want to:\n");
+		printf("\t1 - know a definion of something?\n");
+		printf("\t2 - see a difference between to things?\n");
+		printf("\t3 - play a game with me?\n");
+		printf("\t4 - get my full knowledge?\n");
+		printf("\tt - say goodbye?\n");
+
+		scanf("%d", &choice);
+		getchar();
+		switch (choice){
+			
+			case 1: definition_mode();
+					break;
+			case 2: comaparison_mode();
+					break;
+			case 3: guessing_mode();
+					break;
+			case 4: show_tree();
+					break;
+			case 5: return 0;
+		}
+
+	} while (true);
+
 }
--- Akinator.hpp
+++ Akinator.hpp
@@ -3,9 +3,9 @@
 #include "../Tree/Tree.cpp"
 
 
-FILE* data_source = fopen("tree.txt", "rw");
+char tree_filename[] = "tree.txt";
 const int MAX_BUFF_SIZE = 512;
 
-void ask_question(Tree::tree_t* answers, Tree::node_t* cur);
-void make_guess(Tree::tree_t* answers, Tree::node_t* cur);
-void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character);
+void ask_question(Tree::tree_t* answers, Tree::node_t* cur, FILE* data_source);
+void make_guess(Tree::tree_t* answers, Tree::node_t* cur, FILE* data_source);
+void add_new_character(Tree::tree_t* answers, Tree::node_t* old_character, FILE* data_source);
